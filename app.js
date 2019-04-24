f418c3b9f90b767518571c6d364889ef1b633136
Saurabh Modh <saurabhmodh94@gmail.com>
2019-04-24 10:26:06 +0530

    Custom Middleware

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express'); // TODO: ES6
 
 const app = express();
 
+// Custom Middleware
+app.use(function(req, res, next) {
+  // code here
+  next();
+});
+
 // Index Route
 app.get('/', (req, res) => {
   res.send('index');
