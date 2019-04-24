ac1c47c3b309a1c68974b01f7f08c89c714feb43
Saurabh Modh <saurabhmodh94@gmail.com>
2019-04-24 22:17:31 +0530

    bcrypt register password config

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 
 // Load Idea Model
@@ -47,16 +48,26 @@ router.post('/register', (req, res) => {
           email: req.body.email,
           password: req.body.password
         });
-        newUser
-          .save()
-          .then(user => {
-            req.flash('success_msg', 'You are now registered and can log in');
-            res.redirect('/users/login');
-          })
-          .catch(err => {
-            console.log(err);
-            return;
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => {
+                req.flash(
+                  'success_msg',
+                  'You are now registered and can log in'
+                );
+                res.redirect('/users/login');
+              })
+              .catch(err => {
+                console.log(err);
+                return;
+              });
           });
+        });
       }
     });
   }
